4645ee760403e66703733584efe5ec025b5e8069
Jakson Rochelly <jrochelly@gmail.com>
2013-08-28 14:29:37 -0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sacha::Application.config.secret_key_base = '1168905ddbb8e478768ad472c71887d5bb29015cb3640b2f029b9ddc4ec5ea2ffb871553604471be43aba4a4e11db382f61bf1a20ba99d90b07e0842cdbdf5f7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sacha::Application.config.session_store :cookie_store, key: '_sacha_session'
