c3eac6a98f266c1c9d883296dee86fc35d067adc
Jakson Rochelly <jrochelly@gmail.com>
2013-09-13 15:12:28 -0300

    Translate tinymce to portuguese

---
+++ app/assets/javascripts/tinymce/plugins/jbimages/ci/application/language/portuguese/jbstrings_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['jb_blankpage_message'] = "O processo de envio não começou, ou talvez iniciou, mas ainda não terminou, ou ainda seu navegador não conseguiu encontrar o servidor remoto.";
+
+/* End of file jbstrings_lang.php */
---
+++ app/assets/javascripts/tinymce/plugins/jbimages/ci/system/language/portuguese/upload_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['upload_userfile_not_set'] = "Não pode encontrar uma variável chamada userfile.";
+$lang['upload_file_exceeds_limit'] = "O arquivo enviado excede o limite tamanho máximo.";
+$lang['upload_file_exceeds_form_limit'] = "O arquivo enviado excede o limite tamanho máximo pelo formulário.";
+$lang['upload_file_partial'] = "O arquivo foi enviado partialmente.";
+$lang['upload_no_temp_directory'] = "Pasta temporária faltando.";
+$lang['upload_unable_to_write_file'] = "O arquivo não pode ser escrivo no disco.";
+$lang['upload_stopped_by_extension'] = "O arquivo enviado foi impedido pela extensão.";
+$lang['upload_no_file_selected'] = "Você não selecionou um arquivo para enviar.";
+$lang['upload_invalid_filetype'] = "O tipo de arquivo selecionado não é permitido.";
+$lang['upload_invalid_filesize'] = "O arquivo que você está tentando enviar é maior que o permitido.";
+$lang['upload_invalid_dimensions'] = "A imagem que você está tentando enviar tem resolução maior que o permitido.";
+$lang['upload_destination_error'] = "Um problema foi encontrada enquanto movia o arquivo enviado para o destino final.";
+$lang['upload_no_filepath'] = "O caminho final do arquivo não parece ser válido.";
+$lang['upload_no_file_types'] = "Você não especificou qualquer tipo de arquivo permitido.";
+$lang['upload_bad_filename'] = "O nome do arquivo que você enviou já existe no servidor.";
+$lang['upload_not_writable'] = "A pasta de destino parece não ter permissão de escrita.";
+
+
+/* End of file upload_lang.php */
