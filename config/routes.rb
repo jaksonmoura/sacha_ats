f725aedde45a58fe9f59cf816254166effd35dfe
Jakson Rochelly <jrochelly@gmail.com>
2013-09-13 11:00:01 -0300

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,56 +1,17 @@
 Sacha::Application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  scope '/sacha' do
+    match '/login' => 'sessions#create', via: [:get, :post]
+    match '/logout' => 'sessions#destroy', via: [:get, :post]
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+    resources :technicals
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+    resources :calls
+    get '/calls/:id/close/' => 'calls#close', as: :close
+    get '/list/' => 'calls#index', as: :list
+    match '/calls/:id/done/' => 'calls#done', as: :done, via: [:patch, :put]
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+    root 'welcomes#index'
+  end
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
   
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
